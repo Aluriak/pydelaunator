9d01ddceb54e830cfdcd409b87c4a2f1959cc0cc
Aluriak <lucas.bourneuf@laposte.net>
2017-04-03 00:57:26 +0200

    placer: fully functionnal API
    Takes care of ubiquists elements and expose a poc of a fully usable API.
    
    Maybe not the most optimized, but a mapping object->vertex with a dict
    is certainly the less invasive for end-user.

--- placer.py
+++ placer.py
@@ -1,5 +1,6 @@
 
 
+import navigation
 from mesh import Mesh
 from vertex import Vertex
 from commons import logger
@@ -14,6 +15,28 @@ class Placer:
 
     def __init__(self, width:int, height:int):
         self.mesh = Mesh(width, height)
+        self._objects = {}  # client object: vertex in mesh
 
-    def add(self, obj:object, x:int, y:int):
-        self.mesh.add()
+    def add(self, obj:object, x:int, y:int) -> object:
+        assert obj not in self._objects
+        if obj in self._objects:
+            i, j = self._objects[obj]
+            logger.warning("Placer can't add object '{}' because it is already "
+                           "placed (at ({};{}). It will be moved instead."
+                           "".format(obj, i, j))
+            self.move(obj, x - i, y - j)
+        else:  # object is new
+            added = self.mesh.add(obj, x, y)
+            self._objects[obj] = added
+            return obj
+
+    def move(self, obj:object, dx:int, dy:int):
+        self.mesh.move(self._objects[obj], dx, dy)
+
+    def nearests(self, obj:object) -> iter:
+        """Yield the nearests objects."""
+        yield from (vertex.payload for vertex in navigation.nearests(self._objects[obj]))
+
+
+if __name__ == "__main__":
+    pass
